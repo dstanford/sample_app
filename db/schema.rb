47bc063d839ce66dc4efcb8229645836d7132eac
Daniel Stanford <daniel@danielstanford.com>
2011-09-23 23:38:41 -0500

    added user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110923004825) do
+ActiveRecord::Schema.define(:version => 20110923221320) do
 
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password"
-    t.string   "salt"
-    t.boolean  "admin",              :default => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string    "name"
+    t.string    "email"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "encrypted_password"
+    t.string    "salt"
+    t.boolean   "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -72,12 +72,12 @@ describe UsersController do
       assigns(:user).should == @user
     end
 	
-	it "should have the right title" do
-	  get :show, :id => @user
-	  response.should have_selector("title", :content => @user.name)
-	end
+	  it "should have the right title" do
+	    get :show, :id => @user
+	    response.should have_selector("title", :content => @user.name)
+	  end
 	
-	it "should include the user's name" do
+	  it "should include the user's name" do
       get :show, :id => @user
       response.should have_selector("h1", :content => @user.name)
     end
@@ -87,6 +87,13 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
 	
+	  it "should show the user's microposts" do
+	    mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+	    mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+	    get :show, :id => @user
+	    response.should have_selector("span.content", :content => mp1.content)
+	    response.should have_selector("span.content", :content => mp2.content)
+    end
   end
   
   
